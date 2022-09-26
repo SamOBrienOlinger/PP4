768cb22754ea6ebdc4451705ec8ddaeff82dfc3b
SamOBrienOlinger <samtobo@yahoo.co.uk>
2022-09-26 12:46:21 +0000

    js folder and script.js file added to assets folder

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,21 @@
-let userLoggedIn = false;
+/*let userLoggedIn = false;
 if (userLoggedIn) {
   alert('Welcome! You have logged in.');
 } else {
   alert('Please log in.');
 }
+
+let generateCurse = function (e) {
+    if (!checkName()) {
+      return;
+    }
+
+function checkName() {
+    if (document.getElementById("login").value !== "") {
+      
+      return true;
+    } 
+  
+    alert("please login);
+      return false;
+  }*/
